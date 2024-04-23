4c1a3d769bd809d6c676709cfae5a2b32daa5b5e
Haleigh <haleighvalentine@gmail.com>
2024-04-23 22:52:18 +0200

    Set showSideBar property to false on initialisation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { initialData } from './initialData.js';
 function initializeData() {
     if (!localStorage.getItem('tasks')) {
         localStorage.setItem('tasks', JSON.stringify(initialData));
-        localStorage.setItem('showSideBar', 'true');
+        localStorage.setItem('showSideBar', 'false');
     } else {
         console.log('Data already exists in localStorage');
     }
